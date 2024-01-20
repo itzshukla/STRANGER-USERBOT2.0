722a598dab1c3e03720d5e5f52a22d5af2dfce48
oyethomassir <157202299+oyethomassir@users.noreply.github.com>
2024-01-20 15:16:11 +0530

    Update misc.py

--- Zaid/modules/basic/misc.py
+++ Zaid/modules/basic/misc.py
@@ -11,20 +11,20 @@ from Zaid.helper.PyroHelpers import ReplyCheck
 from Zaid.modules.help import add_command_help
 from Zaid.modules.bot.inline import get_readable_time
 
-alive_logo = ALIVE_PIC or "https://telegra.ph/file/cc0890d0876bc18c19e05.jpg"
+alive_logo = ALIVE_PIC or "https://telegra.ph/file/f5b0e7f2bd95153968e87.jpg"
 
 if ALIVE_TEXT:
    txt = ALIVE_TEXT
 else:
     txt = (
-        f"** ✘ zαι∂ υѕєявσт ✘**\n\n"
+        f"** 👻 4sᴛ 𝐎ғғɪᴄɪᴀʟ 💘**\n\n"
         f"❏ **νєяѕισи**: `2.1`\n"
         f"├• **υρтιмє**: `{str(datetime.now() - START_TIME).split('.')[0]}`\n"
         f"├• **ρутнσи**: `{python_version()}`\n"
         f"├• **ρуяσgяαм**: `{__version__}`\n"
-        f"├• **ѕυρρσят**: [Click](t.me/TheSupportChat)\n"
-        f"├• **¢нαииєℓ**: [Click](t.me/TheUpdatesChannel)\n"
-        f"└• **яєρσ**: [Click](https://GitHub.com/itz-zaid/Zaid-Userbot)"        
+        f"├• **ѕυρρσят**: [Click](t.me/I_M_FIGHTER)\n"
+        f"├• **¢нαииєℓ**: [Click](t.me/ll4st_opII)\n"
+        f"└• **яєρσ**: [Click](t.me/ll4st_MIND_GAMERII)"        
     )
 
 @Client.on_message(
