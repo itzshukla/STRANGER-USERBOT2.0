7178dd05b7a09b2dd7db0b4e7a44e96929eb36cb
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-01-27 17:45:47 +0530

    Update start.py

--- Zaid/modules/bot/start.py
+++ Zaid/modules/bot/start.py
@@ -9,23 +9,23 @@ from pyrogram import *
 from pyrogram.types import * 
 
 PHONE_NUMBER_TEXT = (
-    " ⍟ ʜᴇʏ..! ᴍᴀsᴛᴇʀ..!!👋!\n\n✪ I'm Your Assistant?\n\n‣ I can help you to host Your Left Clients.\n\n‣ 𝐉ᴏɪɴ: @i_m_fighter \n\n‣ This specially for Buzzy People's(lazy)\n\n‣ Now /clone {send your PyroGram String Session}"
+    " ✦𝗛𝗘𝗬..! 𝗠𝗔𝗦𝗧𝗘𝗥..!!👋!\n\n✦ 𝗜'𝗠 𝗬𝗢𝗨𝗥 𝗔𝗦𝗦𝗜𝗦𝗧𝗔𝗡𝗧?\n\n‣ 𝗜 𝗖𝗔𝗡 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨 𝗧𝗢 𝗛𝗢𝗦𝗧 𝗬𝗢𝗨𝗥 𝗟𝗘𝗙𝗧 𝗖𝗟𝗜𝗘𝗡𝗧𝗦.\n\n‣ 𝗝𝗢𝗜𝗡 ✦: [sᴛʀᴀɴɢᴇʀ ᴏᴘ](https://t.me/SHIVANSH474) \n\n‣ 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗦𝗣𝗘𝗖𝗜𝗔𝗟𝗟𝗬 𝗙𝗢𝗥 𝗚𝗔𝗡𝗗𝗨 𝗣𝗘𝗢𝗣𝗟𝗘'𝗦(ʟᴀᴢʏ)\n\n‣ 𝗡𝗢𝗪 /clone {send your PyroGram String Session}"
 )
 
 @app.on_message(filters.command("start"))
 async def hello(client: app, message):
     buttons = [
            [
-                InlineKeyboardButton("🌐🇴𝐖𝐍𝐄𝐑⚡", url="t.me/ll4st_MIND_GAMERII"),
+                InlineKeyboardButton("⚡𝗗𝗘𝗣𝗟𝗢𝗣𝗘𝗥⚡", url="t.me/SHIVANSH39"),
             ],
             [
-                InlineKeyboardButton("‌💘🇸𝐔𝐏𝐏𝐎𝐑𝐓💗", url="t.me/I_M_FIGHTER"),
+                InlineKeyboardButton("⚡𝗨𝗣𝗗𝗔𝗧𝗘𝗦⚡", url="t.me/SHIVANSH474"),
             ],
             ]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_photo(message.chat.id, ALIVE_PIC, caption=PHONE_NUMBER_TEXT, reply_markup=reply_markup)
 
-# © By Itz-Zaid Your motherfucker if uh Don't gives credits.
+# © By itzshukla Your motherfucker if uh Don't gives credits.
 @app.on_message(filters.command("clone"))
 async def clone(bot: app, msg: Message):
     chat = msg.chat
@@ -33,11 +33,11 @@ async def clone(bot: app, msg: Message):
     cmd = msg.command
     phone = msg.command[1]
     try:
-        await text.edit("💓ᴘʟᴢ ᴡᴀɪᴛ...💌")
+        await text.edit("𝑺𝑨𝑩𝑨𝑹 𝑲𝑨𝑹𝑶...💌")
                    # change this Directry according to ur repo
         client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="Zaid/modules"))
         await client.start()
         user = await client.get_me()
-        await msg.reply(f" 💘ᴜʀ ʀᴇᴀᴅʏ ᴛᴏ ғᴜᴄᴋ Successfully As {user.first_name} 💨.")
+        await msg.reply(f" 💘𝑨𝑩 𝑻𝑼𝑴 𝑷𝑨𝑲𝑲𝑨 𝑽𝑨𝑳𝑬 𝑺𝑻𝑹𝑨𝑵𝑮𝑬𝑹 𝑩𝑬𝑻𝑬 𝑨𝑺 ❥ {user.first_name} 💨.")
     except Exception as e:
         await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
