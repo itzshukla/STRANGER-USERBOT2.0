004e1a929bc640073e607ebec303df3d600801cc
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-01-28 09:23:38 +0530

    Update clone.py

--- Zaid/modules/basic/clone.py
+++ Zaid/modules/basic/clone.py
@@ -8,8 +8,8 @@ from Zaid.helper.basic import edit_or_reply, get_text, get_user
 
 from Zaid.modules.help import *
 
-OWNER = os.environ.get("OWNER", "✰𝕊𝕋ℝ𝔸ℕ𝔾𝔼ℝ✰")
-BIO = os.environ.get("BIO", "💕ɪ ᴀᴍ sᴛʀᴀɴɢᴇʀ💕")
+OWNER = os.environ.get("OWNER", "⊷𓆩🇸​𝗧𝗥𝗔𝗡𝗚𝗘𝗥•─‌⃛≛⃝🦅 ༆𝐗𝐃𐏓")
+BIO = os.environ.get("BIO", "💕ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ sᴛʀᴀɴɢᴇʀ💕")
 
 
 @Client.on_message(filters.command("clone", ".") & filters.me)
