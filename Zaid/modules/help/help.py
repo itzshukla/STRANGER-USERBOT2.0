cf504505967be4ae581ddd3f331150aeb3c959c2
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-03-30 08:06:42 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -44,7 +44,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @SHIVANSH474 × @MASTIWITHFRIENDSX •",
+                f"```{str(ac)}```\n• @SHIVANSH474 × @STRANGERXWORLD •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @SHIVANSH474 × @MASTIWITHFRIENDSX •"
+            message, f"```{str(ac)}```\n• @SHIVANSH474 × @STRANGERXWORLD •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
